bc123dd7f0eb720cf3f9834c6d8b9ae5530b71c9
furqonStudio <furqonstudio@gmail.com>
2025-05-25 16:50:56 +0700

    feat: update component

--- components/DataTable.tsx
+++ components/DataTable.tsx
@@ -1,38 +1,28 @@
 'use client'
 
 import {
-  ColumnDef,
-  ColumnFiltersState,
-  SortingState,
-  VisibilityState,
-  flexRender,
+  Table,
+  TableHeader,
+  TableBody,
+  TableRow,
+  TableHead,
+  TableCell,
+} from '@/components/ui/table'
+import {
   getCoreRowModel,
-  getFacetedRowModel,
-  getFacetedUniqueValues,
   getFilteredRowModel,
   getPaginationRowModel,
   getSortedRowModel,
   useReactTable,
+  flexRender,
+  ColumnDef,
+  ColumnFiltersState,
+  SortingState,
+  VisibilityState,
 } from '@tanstack/react-table'
-
 import { useState } from 'react'
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from '@/components/ui/table'
+import { Checkbox } from '@/components/ui/checkbox'
 import { Button } from '@/components/ui/button'
-import { Label } from '@/components/ui/label'
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from '@/components/ui/select'
 import {
   IconChevronLeft,
   IconChevronRight,
@@ -40,17 +30,15 @@ import {
   IconChevronsRight,
 } from '@tabler/icons-react'
 
-type DataTableProps<TData extends { id: string | number }> = {
-  columns: ColumnDef<TData, unknown>[]
+interface DataTableProps<TData, TValue> {
   data: TData[]
-  getRowId?: (row: TData) => string
+  columns: ColumnDef<TData, TValue>[]
 }
 
-export function DataTable<TData extends { id: string | number }>({
-  columns,
+export function DataTable<TData extends { id: string | number }, TValue>({
   data,
-  getRowId = (row) => row.id.toString(),
-}: DataTableProps<TData>) {
+  columns,
+}: DataTableProps<TData, TValue>) {
   const [rowSelection, setRowSelection] = useState({})
   const [columnVisibility, setColumnVisibility] = useState<VisibilityState>({})
   const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([])
@@ -60,9 +48,36 @@ export function DataTable<TData extends { id: string | number }>({
     pageSize: 10,
   })
 
+  const selectColumn: ColumnDef<TData> = {
+    id: 'select',
+    header: ({ table }) => (
+      <div className="flex items-center justify-center">
+        <Checkbox
+          checked={
+            table.getIsAllPageRowsSelected() ||
+            (table.getIsSomePageRowsSelected() && 'indeterminate')
+          }
+          onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+          aria-label="Select all"
+        />
+      </div>
+    ),
+    cell: ({ row }) => (
+      <div className="flex items-center justify-center">
+        <Checkbox
+          checked={row.getIsSelected()}
+          onCheckedChange={(value) => row.toggleSelected(!!value)}
+          aria-label="Select row"
+        />
+      </div>
+    ),
+    enableSorting: false,
+    enableHiding: false,
+  }
+
   const table = useReactTable({
     data,
-    columns,
+    columns: [selectColumn, ...columns],
     state: {
       sorting,
       columnVisibility,
@@ -70,7 +85,7 @@ export function DataTable<TData extends { id: string | number }>({
       columnFilters,
       pagination,
     },
-    getRowId,
+    getRowId: (row: TData) => row.id?.toString(),
     enableRowSelection: true,
     onRowSelectionChange: setRowSelection,
     onSortingChange: setSorting,
@@ -81,8 +96,6 @@ export function DataTable<TData extends { id: string | number }>({
     getFilteredRowModel: getFilteredRowModel(),
     getPaginationRowModel: getPaginationRowModel(),
     getSortedRowModel: getSortedRowModel(),
-    getFacetedRowModel: getFacetedRowModel(),
-    getFacetedUniqueValues: getFacetedUniqueValues(),
   })
 
   return (
@@ -93,7 +106,7 @@ export function DataTable<TData extends { id: string | number }>({
             {table.getHeaderGroups().map((headerGroup) => (
               <TableRow key={headerGroup.id}>
                 {headerGroup.headers.map((header) => (
-                  <TableHead key={header.id} colSpan={header.colSpan}>
+                  <TableHead key={header.id}>
                     {header.isPlaceholder
                       ? null
                       : flexRender(
@@ -105,8 +118,8 @@ export function DataTable<TData extends { id: string | number }>({
               </TableRow>
             ))}
           </TableHeader>
-          <TableBody className="**:data-[slot=table-cell]:first:w-8">
-            {table.getRowModel().rows?.length ? (
+          <TableBody>
+            {table.getRowModel().rows.length ? (
               table.getRowModel().rows.map((row) => (
                 <TableRow
                   key={row.id}
@@ -125,7 +138,7 @@ export function DataTable<TData extends { id: string | number }>({
             ) : (
               <TableRow>
                 <TableCell
-                  colSpan={columns.length}
+                  colSpan={columns.length + 1}
                   className="h-24 text-center"
                 >
                   No results.
@@ -136,32 +149,13 @@ export function DataTable<TData extends { id: string | number }>({
         </Table>
       </div>
 
+      {/* Footer: Pagination */}
       <div className="flex items-center justify-between px-4">
         <div className="text-muted-foreground hidden flex-1 text-sm lg:flex">
           {table.getFilteredSelectedRowModel().rows.length} of{' '}
           {table.getFilteredRowModel().rows.length} row(s) selected.
         </div>
         <div className="flex w-full items-center gap-8 lg:w-fit">
-          <div className="hidden items-center gap-2 lg:flex">
-            <Label htmlFor="rows-per-page" className="text-sm font-medium">
-              Rows per page
-            </Label>
-            <Select
-              value={`${table.getState().pagination.pageSize}`}
-              onValueChange={(value) => table.setPageSize(Number(value))}
-            >
-              <SelectTrigger size="sm" className="w-20" id="rows-per-page">
-                <SelectValue />
-              </SelectTrigger>
-              <SelectContent side="top">
-                {[10, 20, 30, 40, 50].map((pageSize) => (
-                  <SelectItem key={pageSize} value={`${pageSize}`}>
-                    {pageSize}
-                  </SelectItem>
-                ))}
-              </SelectContent>
-            </Select>
-          </div>
           <div className="flex w-fit items-center justify-center text-sm font-medium">
             Page {table.getState().pagination.pageIndex + 1} of{' '}
             {table.getPageCount()}
@@ -169,7 +163,8 @@ export function DataTable<TData extends { id: string | number }>({
           <div className="ml-auto flex items-center gap-2 lg:ml-0">
             <Button
               variant="outline"
-              className="hidden h-8 w-8 p-0 lg:flex"
+              size="icon"
+              className="size-8"
               onClick={() => table.setPageIndex(0)}
               disabled={!table.getCanPreviousPage()}
             >
@@ -177,6 +172,7 @@ export function DataTable<TData extends { id: string | number }>({
             </Button>
             <Button
               variant="outline"
+              size="icon"
               className="size-8"
               onClick={() => table.previousPage()}
               disabled={!table.getCanPreviousPage()}
@@ -185,6 +181,7 @@ export function DataTable<TData extends { id: string | number }>({
             </Button>
             <Button
               variant="outline"
+              size="icon"
               className="size-8"
               onClick={() => table.nextPage()}
               disabled={!table.getCanNextPage()}
@@ -193,7 +190,8 @@ export function DataTable<TData extends { id: string | number }>({
             </Button>
             <Button
               variant="outline"
-              className="hidden size-8 lg:flex"
+              size="icon"
+              className="size-8"
               onClick={() => table.setPageIndex(table.getPageCount() - 1)}
               disabled={!table.getCanNextPage()}
             >
---
+++ features/post/components/PostDataTable.tsx
@@ -0,0 +1,85 @@
+import { DataTable } from '@/components/DataTable'
+import { Badge } from '@/components/ui/badge'
+import { Button } from '@/components/ui/button'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu'
+import { posts } from '@/data'
+import { IconDotsVertical } from '@tabler/icons-react'
+import { ColumnDef } from '@tanstack/react-table'
+import { z } from 'zod'
+
+const articleSchema = z.object({
+  id: z.number(),
+  title: z.string(),
+  publishedAt: z.string(),
+  category: z.string(),
+  author: z.string(),
+})
+
+const columns: ColumnDef<z.infer<typeof articleSchema>>[] = [
+  {
+    accessorKey: 'title',
+    header: 'Title',
+    cell: ({ row }) => (
+      <div className="font-medium">{row.getValue('title')}</div>
+    ),
+    enableHiding: false,
+  },
+  {
+    accessorKey: 'publishedAt',
+    header: 'Published At',
+    cell: ({ row }) => {
+      const date = new Date(row.getValue('publishedAt'))
+      return (
+        <div className="text-muted-foreground">{date.toLocaleDateString()}</div>
+      )
+    },
+  },
+  {
+    accessorKey: 'category',
+    header: 'Category',
+    cell: ({ row }) => (
+      <Badge variant="outline">{row.getValue('category')}</Badge>
+    ),
+  },
+  {
+    accessorKey: 'author',
+    header: 'Author',
+    cell: ({ row }) => <div>{row.getValue('author')}</div>,
+  },
+  {
+    id: 'actions',
+    cell: () => (
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button
+            variant="ghost"
+            className="data-[state=open]:bg-muted text-muted-foreground flex size-8"
+            size="icon"
+          >
+            <IconDotsVertical />
+            <span className="sr-only">Open menu</span>
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end" className="w-32">
+          <DropdownMenuItem>Edit</DropdownMenuItem>
+          <DropdownMenuItem>Make a copy</DropdownMenuItem>
+          <DropdownMenuItem>Favorite</DropdownMenuItem>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem className="text-destructive">
+            Delete
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    ),
+  },
+]
+
+export default function PostDataTable() {
+  return <DataTable columns={columns} data={posts} />
+}
